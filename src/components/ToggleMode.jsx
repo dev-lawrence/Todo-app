05ed98123d658ed27d4ee3d66ab5598be7690d28
dev-lawrence <lawrenceamrasakpare19@gmail.com>
2023-05-14 21:09:23 +0100

    final commit

--- src/components/ToggleMode.jsx
+++ src/components/ToggleMode.jsx
@@ -2,43 +2,79 @@ import { useEffect } from 'react';
 import iconMoon from '../assets/icon-moon.svg';
 import iconSun from '../assets/icon-sun.svg';
 
-const storeMode = localStorage.getItem('mode');
-
 const Togglemode = ({ darkMode, setDarkMode }) => {
-  const handleToggleMode = () => {
-    if (!darkMode) {
+  const storedMode = localStorage.getItem('mode');
+
+  // enable dark mode
+  const enableDarkMode = () => {
+    document.body.className = 'dark';
+    localStorage.setItem('mode', 'dark');
+  };
+
+  // enable light mode
+  const enableLightMode = () => {
+    document.body.className = 'light';
+    localStorage.setItem('mode', 'light');
+  };
+
+  // handle the media query mode from the system color settings
+  const handleMediaQueryChange = (event) => {
+    const isMediaMatched = event.matches;
+    setDarkMode(isMediaMatched);
+
+    if (event.matches) {
       enableDarkMode();
     } else {
       enableLightMode();
     }
   };
 
-  const enableDarkMode = () => {
-    localStorage.setItem('mode', 'dark');
-    document.body.className = 'dark';
-    setDarkMode(true);
-  };
-
-  const enableLightMode = () => {
-    localStorage.setItem('mode', 'light');
-    document.body.className = 'light';
-    setDarkMode(false);
-  };
-
   useEffect(() => {
-    if (storeMode === 'dark') {
-      enableDarkMode();
-    } else if (storeMode === 'light') {
-      enableLightMode();
-    }
+    const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+    // Initial check
+    handleMediaQueryChange(mediaQuery);
+
+    // Add event listener
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+    // Clean up listener
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange);
+    };
   }, []);
 
+  useEffect(() => {
+    const preferredScheme = window.matchMedia('(prefers-color-scheme: dark)')
+      .matches
+      ? 'dark'
+      : 'light';
+
+    if (storedMode === 'dark') {
+      enableDarkMode();
+      setDarkMode(true);
+    } else if (storedMode === 'light') {
+      enableLightMode();
+      setDarkMode(false);
+    } else {
+      setDarkMode(preferredScheme === 'dark');
+    }
+  }, [storedMode]);
+
+  const handleToggleMode = () => {
+    const newMode = !darkMode;
+    setDarkMode(newMode);
+    if (newMode) {
+      enableDarkMode();
+    } else {
+      enableLightMode();
+    }
+  };
+
   return (
-    <>
-      <div onClick={handleToggleMode} className="toggle-switch">
-        <img src={darkMode ? iconMoon : iconSun} alt="Mode button" />
-      </div>
-    </>
+    <div onClick={handleToggleMode} className="toggle-switch">
+      <img src={darkMode ? iconSun : iconMoon} alt="Mode button" />
+    </div>
   );
 };
 
